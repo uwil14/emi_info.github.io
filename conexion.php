4ff2681bc41d98e14a4c86aff594be50fd4e08ef
uwil14 <50802423+uwil14@users.noreply.github.com>
2021-06-28 14:24:42 -0400

    Add files via upload

---
+++ conexion.php
@@ -0,0 +1,28 @@
+<?php
+// DB.class.php
+
+class Conexion {
+	
+	protected $db_name = 'expocruz';
+	protected $db_user = 'root';
+	protected $db_pass = '';
+	protected $db_host = 'localhost';
+	public $connect_db;
+	
+	// Open a connect to the database.
+	// Make sure this is called on every page that needs to use the database.
+	
+	public function connect() {
+	
+		$connect_db = new mysqli( $this->db_host, $this->db_user, $this->db_pass, $this->db_name );
+		
+		if ( mysqli_connect_errno() ) {
+			printf("Connection failed: %s\
+", mysqli_connect_error());
+			exit();
+		}
+		return true;
+		
+	}
+
+}
---
+++ js/zoom.js
@@ -0,0 +1,9 @@
+$('#imagen').zoomify({
+  // animation duration
+  duration: 200,
+  // easing effect
+  easing:  'linear',
+  // zoom scale
+  // 1 = fullscreen
+  scale:   1 //0.9
+});
